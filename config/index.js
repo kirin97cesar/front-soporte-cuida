712a00805ae26a30cedc46100871f6cd29620a3d
Paulo Carbajal <paulo.carbajal@cuida.pe>
2025-07-29 12:54:53 -0500

    loading agregado...

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 // Define una constante global PROD
-//window.API_BASE_URL = 'https://soportes.cooperativasanviator.org.pe/api/';
+window.API_BASE_URL = 'https://soportes.cooperativasanviator.org.pe/api/';
 
 // Define una constante global TEST
-window.API_BASE_URL = 'http://127.0.0.1/api/';
+//window.API_BASE_URL = 'http://127.0.0.1/api/';

a337f557757926c3269e976d849731a480e6750e
Paulo Carbajal <paulo.carbajal@cuida.pe>
2025-07-31 11:38:56 -0500

    public

--- public/config/index.js
+++ public/config/index.js
@@ -1,5 +1,7 @@
 // Define una constante global PROD
-window.API_BASE_URL = 'https://soportes.cooperativasanviator.org.pe/api/';
+//window.API_BASE_URL = 'https://soportes.cooperativasanviator.org.pe/api/';
+window.API_BASE_URL = 'https://backend-soporte-cuida.onrender.com/';
+
 
 // Define una constante global TEST
 //window.API_BASE_URL = 'http://127.0.0.1/api/';

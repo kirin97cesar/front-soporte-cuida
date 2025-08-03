b8b469410d10f768b5595ac05cb118d8dbfd6958
Paulo Carbajal <paulo.carbajal@cuida.pe>
2025-08-03 12:27:59 -0500

    redireccion y soportes

--- public/config/index.js
+++ public/config/index.js
@@ -1,7 +1,34 @@
 // Define una constante global PROD
-//window.API_BASE_URL = 'https://soportes.cooperativasanviator.org.pe/api/';
 window.API_BASE_URL = 'https://backend-soporte-cuida.onrender.com/';
 
 
 // Define una constante global TEST
 //window.API_BASE_URL = 'http://127.0.0.1/api/';
+
+
+const registrarSoporte = async ({ tipo, payload }) => {
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("X-Auth-Token", localStorage.getItem('token'));
+
+    const raw = JSON.stringify({
+        tipo,
+        payload
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch(`${API_BASE_URL}public/soporte.php`, requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
+
+}
+
+window.REGISTRAR_SOPORTE = registrarSoporte;

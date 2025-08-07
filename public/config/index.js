cbe5a23c350109a27962d92f2436feea8d3cfdb8
Paulo Carbajal <paulo.carbajal@cuida.pe>
2025-08-07 18:45:40 -0500

    clasificacion de valor

--- public/config/index.js
+++ public/config/index.js
@@ -1,9 +1,9 @@
 // Define una constante global PROD
-window.API_BASE_URL = 'https://backend-soporte-cuida.onrender.com/';
+//window.API_BASE_URL = 'https://backend-soporte-cuida.onrender.com/';
 
 
 // Define una constante global TEST
-//window.API_BASE_URL = 'http://127.0.0.1/api/';
+window.API_BASE_URL = 'http://127.0.0.1/api/';
 
 
 const registrarSoporte = async ({ tipo, payload }) => {

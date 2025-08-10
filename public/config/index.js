6696a2fcf630a82b5e81963b0da034b7a22de0d5
Paulo Carbajal <paulo.carbajal@cuida.pe>
2025-08-09 22:17:11 -0500

    test

--- public/config/index.js
+++ public/config/index.js
@@ -1,9 +1,76 @@
 // Define una constante global PROD
 window.API_BASE_URL = 'https://backend-soporte-cuida.onrender.com/';
 
-
 // Define una constante global TEST
 //window.API_BASE_URL = 'http://127.0.0.1/api/';
+
+
+async function cargarComponente(nombre, idDestino) {
+    try {
+        const url = '../components/aside.html?v=' + Date.now(); // evita cache
+        const res = await fetch(url);
+        const html = await res.text();
+        document.getElementById(idDestino).insertAdjacentHTML('beforeend', html);
+    } catch (err) {
+        console.error(`Error cargando ${nombre}:`, err);
+    } finally {
+        const informacionUser = leerDesdeLocalStorage(
+          "user",
+          localStorage.getItem("token"),
+        );
+
+        if (informacionUser) {
+          const jsonInfo = JSON.parse(informacionUser);
+          document.getElementById("userEmail").innerText = jsonInfo.email;
+          document.getElementById("avatarInicial").innerText = jsonInfo.email[0].toUpperCase();
+        }
+
+        const info = localStorage.getItem('detalle');
+        const detalle = JSON.parse(info);
+
+        if(!detalle || ( detalle && detalle.secciones.length == 0) ) {
+          localStorage.clear();
+          window.location.href = "../index.html";
+          return;
+        }
+
+        for (const seccion of detalle.secciones) {
+          if(seccion == 'PRINCIPAL') {
+            document.getElementById('menu-principal').style.display = 'block';
+          }
+
+          if(seccion == 'PRODUCTOS') {
+            document.getElementById('menu-producto').style.display = 'block';
+          }
+
+          if(seccion == 'AGENTES') {
+            document.getElementById('menu-agentes').style.display = 'block';
+          }
+
+          if(seccion == 'MOTORIZADOS') {
+            document.getElementById('menu-motorizado').style.display = 'block';
+          }
+          
+          if(seccion == 'SEGUIMIENTOS') {
+            document.getElementById('menu-seguimiento').style.display = 'block';
+          }
+
+          if(seccion == 'PEDIDOS/SOLICITUDES') {
+            document.getElementById('menu-pedido').style.display = 'block';
+          }
+
+          if(seccion == 'ESQUEMAS') {
+            document.getElementById('menu-esquemas').style.display = 'block';
+          }
+
+          if(seccion == 'CLIENTES') {
+            document.getElementById('menu-cliente').style.display = 'block';
+          }
+        }
+    }
+}
+
+window.CARGAR_COMPONENTE = cargarComponente;
 
 
 const registrarSoporte = async ({ tipo, payload }) => {
